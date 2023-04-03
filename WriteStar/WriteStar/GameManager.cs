12bda6762ff8c7d8cefcbc507ce91b6f2707690c
IdeaBank <isp_lwj@naver.com>
2023-04-03 11:29:16 +0900

    [FIX] 재귀함수를 사용하면 잘못된 입력도 연산하는 오류 수정 및 주석 추가

--- WriteStar/WriteStar/GameManager.cs
+++ WriteStar/WriteStar/GameManager.cs
@@ -6,22 +6,22 @@ namespace WriteStar
     {
         public GameManager()
         {
-            this.starWriter = new StarWriter();
+            this._starWriter = new StarWriter();
             // 내부 변수로 별을 찍어주는 StarWriter 클래스의 인스턴스 생성
         }
 
         public void StartGame()
         {
-            if (this.starWriter.InputType() == false)
+            if (this._starWriter.InputType() == false)
                 return;
 
-            if (this.starWriter.InputLines() == true) // 1 이상을 입력 받았을 때만 출력.
-                this.starWriter.PrintStar();
+            if (this._starWriter.InputLines() == true) // 1 이상을 입력 받았을 때만 출력.
+                this._starWriter.PrintStar();
 
             StartGame();
             // 정상적으로 끝났을 경우에는 다시 시작
         }
 
-        private StarWriter starWriter;
+        private StarWriter _starWriter;
     }
 }
--- WriteStar/WriteStar/StarWriter.cs
+++ WriteStar/WriteStar/StarWriter.cs
@@ -9,39 +9,91 @@ namespace WriteStar
             // 빈 생성자
         }
 
+        public bool IsDigitsOnly(string str)
+        {
+            if (str.Length == 0)
+                return false;
+            
+            foreach (char c in str)
+                if (c < '0' || c > '9')
+                    return false;
+            // 길이가 0이거나, 숫자가 아닌 문자가 있으면 false 반환
+            
+            return true;
+            // 숫자로 이루어졌을 경우에는 true 반환
+        }
+        
         public bool InputType()
         {
-            this.type = Int32.Parse(Console.ReadLine());
-            // 출력할 별의 형태를 입력 받음.
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("*****************************");
+                Console.WriteLine("출력할 별의 형태를 숫자로 입력해주세요.");
+                Console.WriteLine("1. 가운데 정렬 별 찍기");
+                Console.WriteLine("2. 1번의 반대로 찍기");
+                Console.WriteLine("3. 모래시계");
+                Console.WriteLine("4. 다이아");
+                Console.WriteLine("0. 프로그램 종료");
+                Console.WriteLine("*****************************");
 
-            if (!(0 <= this.type && this.type <= 4))
-                this.InputType();
-            // 0~4가 아닐 경우, 계속 입력 받음
+                string tempInput = Console.ReadLine();
 
-            if (this.type == 0)
-                return false;
-            // 0일 경우, 프로그램을 종료하기 위해 false(비정상 입력) 반환
+                if (IsDigitsOnly(tempInput) == false)
+                    continue;
+                // 문자가 있을 경우, 다시 입력 받음
 
-            return true;
-            // 1~4일 경우, 정상 입력으로 판단해 true 반환
+                Console.WriteLine(tempInput);
+
+                this._type = Int32.Parse(tempInput);
+                // 출력할 별의 형태 저장
+
+                if (!(0 <= this._type && this._type <= 4))
+                    continue;
+                // 0~4가 아닐 경우, 다시 입력 받음
+
+                if (this._type == 0)
+                    return false;
+                // 0일 경우, 프로그램을 종료하기 위해 false(비정상 입력) 반환
+
+                return true;
+                // 1~4일 경우, 정상 입력으로 판단해 true 반환
+            }
         }
 
         public bool InputLines()
         {
-            this.totalLines = Int32.Parse(Console.ReadLine());
-            // 출력할 별의 형태를 입력 받음.
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("********************");
+                Console.WriteLine("출력할 줄의 수를 입력하세요");
+                Console.WriteLine("********************");
 
-            if (this.totalLines < 0)
-                return false;
-            // 0 이하일 경우, 비정상적인 입력이므로 false 반환
+                string tempInput = Console.ReadLine();
 
-            return true;
-            // 1 이상일 경우, 정상 입력으로 판단해 true 반환
+                if (IsDigitsOnly(tempInput) == false)
+                    continue;
+                // 문자가 있을 경우, 다시 입력 받음
+
+                this._totalLines = Int32.Parse(tempInput);
+                // 출력할 별의 줄 수를 입력 받음.
+
+                if (this._totalLines == 0)
+                    return false;
+                // 0일 경우, 비정상적인 입력이므로 false 반환 (음수일 경우에는 -가 들어가 있으므로 문자로 취급되어, 다시 입력 받음)
+
+                return true;
+                // 1 이상일 경우, 정상 입력으로 판단해 true 반환
+            }
         }
 
         public void PrintStar()
         {
-            switch (type) // 타입에 맞는 별 출력
+            Console.Clear();
+            // 별을 출력하기 위해 미리 콘솔창 깔끔하게 정리
+            
+            switch (_type) // 타입에 맞는 별 출력
             {
                 case 1:
                     PrintPyramid();
@@ -58,13 +110,17 @@ namespace WriteStar
                 default:
                     return;
             }
+            
+            Console.WriteLine("계속하려면 아무 키나 누르세요...");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         public void PrintPyramid()
         {
-            for (int i = 0; i < this.totalLines; ++i)
+            for (int i = 0; i < this._totalLines; ++i)
             {
-                for (int j = 0; j < this.totalLines - i - 1; ++j)
+                for (int j = 0; j < this._totalLines - i - 1; ++j)
                     Console.Write(" ");
                 // 왼쪽 칸 띄워줌
                 
@@ -79,13 +135,13 @@ namespace WriteStar
 
         public void PrintRevPyramid()
         {
-            for (int i = 0; i < this.totalLines; ++i)
+            for (int i = 0; i < this._totalLines; ++i)
             {
                 for (int j = 0; j < i; ++j)
                     Console.Write(" ");
                 // 왼쪽 칸 띄워줌
                 
-                for(int j = 0; j < (this.totalLines - i) * 2 - 1; ++j)
+                for(int j = 0; j < (this._totalLines - i) * 2 - 1; ++j)
                     Console.Write("*");
                 // 별 찍기
                 
@@ -105,9 +161,9 @@ namespace WriteStar
         {
             // 모래시계와 달리, 가운데 있는 줄은 한 번만 등장하기 때문에 직접 출력
             
-            for (int i = 0; i < this.totalLines; ++i)
+            for (int i = 0; i < this._totalLines; ++i)
             {
-                for (int j = 0; j < this.totalLines - i - 1; ++j)
+                for (int j = 0; j < this._totalLines - i - 1; ++j)
                     Console.Write(" ");
                 // 왼쪽 칸 띄워줌
                 
@@ -119,13 +175,13 @@ namespace WriteStar
                 // 한 줄 내림
             }
             
-            for (int i = 1; i < this.totalLines; ++i)
+            for (int i = 1; i < this._totalLines; ++i)
             {
                 for (int j = 0; j < i; ++j)
                     Console.Write(" ");
                 // 왼쪽 칸 띄워줌
                 
-                for(int j = 0; j < (this.totalLines - i) * 2 - 1; ++j)
+                for(int j = 0; j < (this._totalLines - i) * 2 - 1; ++j)
                     Console.Write("*");
                 // 별 찍기
                 
@@ -134,7 +190,7 @@ namespace WriteStar
             }
         }
 
-        private int type; // 출력 형태
-        private int totalLines; // 출력할 줄 수
+        private int _type; // 출력 형태
+        private int _totalLines; // 출력할 줄 수
     }
 }
