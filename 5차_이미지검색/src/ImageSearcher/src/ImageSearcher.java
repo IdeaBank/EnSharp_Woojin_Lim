feb341a754000949b9f0fa5bf54040aabf269f5e
IdeaBank <isp_lwj@naver.com>
2023-05-09 14:52:39 +0900

    [ADD] 기본 프로젝트 파일들 추가

---
+++ 5차_이미지검색/src/ImageSearcher/src/ImageSearcher.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class ImageSearcher {
+    public JPanel panel1;
+
+    public ImageSearcher()
+    {
+        
+    }
+
+    public void StartProgram()
+    {
+
+    }
+}
---
+++ 5차_이미지검색/src/ImageSearcher/src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ImageSearcher imageSearcher = new ImageSearcher();
+        imageSearcher.StartProgram();
+    }
+}
