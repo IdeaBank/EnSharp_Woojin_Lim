3c147e9980cd4b72f5563f93007c758fed432d8c
Woojin Lim <isp_lwj@naver.com>
2023-06-05 02:15:25 +0900

    [REFACTOR] CLS 명령어 수정

--- CMD_Windows/src/Command/src/commands/ClearConsole.java
+++ CMD_Windows/src/Command/src/commands/ClearConsole.java
@@ -1,34 +1,34 @@
 package commands;
 
-import commandInterface.CommandInterface;
+import commandInterface.CommandCommonFunctionContainer;
+import commandInterface.ComplexCommandInterface;
 import constant.CommandResultType;
 import model.PromptData;
 import view.PromptView;
 
-public class ClearConsole implements CommandInterface {
+public class ClearConsole extends CommandCommonFunctionContainer implements ComplexCommandInterface {
     @Override
     public void executeCommand(PromptData promptData, String command) {
+        command = getNormalCommand(command, "cls");
         String[] commandToken = getCommandToken(command);
-        if(isCommandValid(command) == CommandResultType.SUCCESS) {
+
+        CommandResultType commandResultType = isCommandValid(commandToken);
+
+        if(commandResultType == CommandResultType.SUCCESS) {
             PromptView.getInstance().clearPrompt();
         }
 
-        else {
-            PromptView.getInstance().printMessage("'" + commandToken[0] +"'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는\n배치 파일이 아닙니다.");
+        else if(commandResultType == CommandResultType.COMMAND_NOT_EXIST) {
+            PromptView.getInstance().printNoCommand(commandToken[0]);
         }
     }
 
     @Override
-    public CommandResultType isCommandValid(String command) {
-        if(command.equals("cls") || command.startsWith("cls.") || command.startsWith("cls\\") || command.startsWith("cls ")) {
+    public CommandResultType isCommandValid(String[] commandToken) {
+        if(commandToken[0].equalsIgnoreCase("cls")) {
             return CommandResultType.SUCCESS;
         }
 
         return CommandResultType.COMMAND_NOT_EXIST;
-    }
-
-    @Override
-    public String[] getCommandToken(String command) {
-        return command.split(" ");
     }
 }
