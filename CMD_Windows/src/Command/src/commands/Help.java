dce0d831158c65dadb6f99ca2fe3ec133c151d8a
Woojin Lim <isp_lwj@naver.com>
2023-06-05 02:16:46 +0900

    [REFACTOR] Help 명령어 수정된 인터페이스에 맞게 수정

--- CMD_Windows/src/Command/src/commands/Help.java
+++ CMD_Windows/src/Command/src/commands/Help.java
@@ -1,38 +1,37 @@
 package commands;
 
-import commandInterface.CommandInterface;
+import commandInterface.CommandCommonFunctionContainer;
+import commandInterface.ComplexCommandInterface;
 import constant.CommandResultType;
 import model.PromptData;
 import view.PromptView;
 
-public class Help implements CommandInterface {
+public class Help extends CommandCommonFunctionContainer implements ComplexCommandInterface {
     @Override
     public void executeCommand(PromptData promptData, String command) {
+        command = getNormalCommand(command, "help");
         String[] commandToken = command.split(" ");
 
+        CommandResultType commandResultType = isCommandValid(commandToken);
+
         // 명령어가 제대로 되어있으면 help 문구 출력
-        if(isCommandValid(command) == CommandResultType.SUCCESS) {
+        if(commandResultType == CommandResultType.SUCCESS) {
             PromptView.getInstance().printHelp();
         }
 
         // 명령어가 이상하면 오류 문구 출력
-        else {
-            PromptView.getInstance().printMessage("'" + commandToken[0] +"'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는\n배치 파일이 아닙니다.");
+        else if(commandResultType == CommandResultType.COMMAND_NOT_EXIST) {
+            PromptView.getInstance().printNoCommand(commandToken[0]);
         }
     }
 
     @Override
-    public CommandResultType isCommandValid(String command) {
+    public CommandResultType isCommandValid(String[] commandToken) {
         // help이거나 help로 시작하고 띄어쓰기가 있으면 정상 명령어로 판단
-        if(command.equals("help") || command.startsWith("help ")) {
+        if(commandToken.length == 1) {
             return CommandResultType.SUCCESS;
         }
 
         return CommandResultType.COMMAND_NOT_EXIST;
-    }
-
-    @Override
-    public String[] getCommandToken(String command) {
-        return command.split(" ");
     }
 }
